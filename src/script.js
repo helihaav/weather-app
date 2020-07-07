a105c4c922cc131aeb699048e53f4b695cc1f49e
helihaav <66223560+helihaav@users.noreply.github.com>
2020-07-07 23:17:14 +0300

    Created the index.html, script.js, style.css

---
+++ src/script.js
@@ -0,0 +1,64 @@
+let now = new Date();
+
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = addZero(now.getMinutes());
+
+console.log(`${day}, ${hours}, ${minutes}`);
+let date = document.querySelector("#day-time");
+date.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchedCity = document.querySelector("#search-input");
+  let showCity = document.querySelector("#main-city");
+  showCity.innerHTML = searchedCity.value;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
+function showWeather(response) {
+  let myCity = document.querySelector("#main-city");
+  myCity.innerHTML = response.data.name;
+
+  let myCityTemp = document.querySelector("#real-temperature");
+  myCityTemp.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+
+  let myCityHum = document.querySelector("#humidity");
+  myCityHum.innerHTML = `Humidity: ${Math.round(response.data.main.humidity)}%`;
+
+  let myCityWind = document.querySelector("#wind");
+  myCityWind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}m/s`;
+}
+
+function weatherSearch() {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let city = `${searchInput.value}`;
+  let apiKey = "99fa2ab95f6627691849388c94fbc8df";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  let h1 = document.querySelector("h1");
+  if (searchInput.value) {
+    h1.innerHTML = `${city}`;
+  }
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
+}
+let searchMyCity = document.querySelector("#search-form");
+searchMyCity.addEventListener("submit", weatherSearch);
